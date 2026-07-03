ed62266e57f98d6f6898f87ef0847fe097810a45
senku-btw <297473694+senku-btw@users.noreply.github.com>
2026-07-03 19:16:15 +0000

    modular system

--- configuration.nix
+++ configuration.nix
@@ -1,46 +1,40 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
+# ~/nix-btw/configuration.nix
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    # Explicitly targeting your baseline file
+    ./hardware/baseline.nix
+  ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nix-btw";
 
-  networking.hostName = "nix-btw"; # Define your hostname.
-
-  # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
-  networking.useDHCP = false;
+  networking.useDHCP = lib.mkDefault true;
 
-  # Enable Flakes and the new 'nix' CLI
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.groot = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-      fastfetch
-    ];
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
   };
 
-  # List packages installed in system profile.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.groot = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [];
+  };
+
   environment.systemPackages = with pkgs; [
     nano
     wget
     git
+    tree
+    fastfetch
   ];
 
   programs.gnupg.agent = {
@@ -48,24 +42,16 @@
     enableSSHSupport = true;
   };
 
-  # -------------------------------------------------------------------
-  # SSH & Firewall Configuration
-  # -------------------------------------------------------------------
   services.openssh = {
     enable = true;
     settings = {
-      # For security, do not allow root to log in via SSH
       PermitRootLogin = "no";
-      # Allow password authentication for user 'groot'
-      PasswordAuthentication = true;
+      PasswordAuthentication = true; 
+      KbdInteractiveAuthentication = true;
     };
   };
 
-  # Open ports in the firewall for SSH
   networking.firewall.allowedTCPPorts = [ 22 ];
 
-  # -------------------------------------------------------------------
-
-  # This option defines the first version of NixOS you have installed on this particular machine.
-  system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05";
 }
