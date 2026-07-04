5f5cd21618c1659196db268a102c013ceac36304
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-04 14:39:51 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
   description = "Minimalist Enterprise NixOS Configuration Flake";
 
   inputs = {
+    # Main NixOS repository tracking the unstable development branch
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Home Manager tracking development branch, locked to system nixpkgs
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,17 +14,23 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    # Changed hostName configuration key from "nix-btw" to "nix"
+    # System derivation for target host 'nix'
     nixosConfigurations."nix" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      
+      # Expose flake inputs to downstream modules natively
+      specialArgs = { inherit inputs; };
+      
       modules = [
+        # Global system configuration base
         ./configuration.nix
+        
+        # Declarative home environment management module
         home-manager.nixosModules.home-manager
         {
+          # Inherit system package configuration and options inside home-manager
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-        
-          home-manager.users.senku-btw = import ./users/senku-btw/home-manager.nix;
         }
       ];
     };
