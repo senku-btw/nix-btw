060b851796f8a006a84eb7958b5623c3d3ecb2ad
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-04 05:56:14 +0000

    Update and rename home-manager.nix to home-manager.nix

---
+++ users/batman/home-manager.nix
@@ -0,0 +1,18 @@
+# ~/nix-btw/users/batman/home-manager.nix
+{ config, pkgs, ... }:
+
+{
+  home.username = "batman";
+  home.homeDirectory = "/home/batman";
+  home.stateVersion = "24.05"; 
+
+  programs.home-manager.enable = true;
+
+  # Create out-of-store developer links directly into your dotfiles tracking folder
+  home.file = {
+    ".config/niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.config/niri/config.kdl";
+    
+    # Symlink Fuzzel configuration directly from your local .dotfiles repository
+    ".config/fuzzel/fuzzel.ini".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.config/fuzzel/fuzzel.ini";
+  };
+}
