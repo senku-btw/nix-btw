0b74943bf56fbe2256b3167c060b1124bc040cb1
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-04 14:43:04 +0000

    Update networking.nix

--- boot/networking.nix
+++ boot/networking.nix
@@ -1,10 +1,16 @@
-# ~/nix-btw/hardware/networking.nix
+# ~/nix-btw/boot/networking.nix
 { config, lib, pkgs, ... }:
 
 {
+  # Define system network identity
   networking.hostName = "nix";
-  networking.networkmanager.enable = true;
-  networking.useDHCP = lib.mkDefault true;
 
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  # Enable NetworkManager for simple Wi-Fi and ethernet management
+  networking.networkmanager.enable = true;
+
+  # Strict firewall configuration
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 ]; # Allow SSH incoming connections
+  };
 }
