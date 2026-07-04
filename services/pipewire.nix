e5fceeed06ed6f6d644e746317fdc2ec65e1b29b
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-04 14:49:02 +0000

    Update pipewire.nix

--- services/pipewire.nix
+++ services/pipewire.nix
@@ -2,18 +2,24 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./wireplumber.nix
-  ];
-
+  # Disable legacy PulseAudio system daemon to prevent hardware resource locking
   hardware.pulseaudio.enable = false;
+
+  # Enable RealtimeKit for system process priority management (prevents audio crackling)
   security.rtkit.enable = true;
 
+  # Core Pipewire multimedia framework orchestration
   services.pipewire = {
     enable = true;
+    
+    # Enable ALSA infrastructure translation layer
     alsa.enable = true;
     alsa.support32Bit = true;
+    
+    # Enable PulseAudio server emulation wrapper
     pulse.enable = true;
+    
+    # Enable JACK audio server API support
     jack.enable = true;
   };
 }
