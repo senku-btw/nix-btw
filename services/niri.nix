51a8c50c6d84769fa955bfb71e102c1a50146797
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-04 06:36:53 +0000

    Create niri.nix

---
+++ services/niri.nix
@@ -0,0 +1,31 @@
+# ~/nix-btw/services/niri.nix
+{ config, pkgs, ... }:
+
+{
+  # Enable Niri Wayland Compositor Infrastructure
+  programs.niri.enable = true;
+
+  # Golden-Standard Wayland & NVIDIA Optimization Variables
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
+    WLR_NO_HARDWARE_CURSORS = "1";     
+    GBM_BACKEND = "nvidia-drm";        
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; 
+    LIBVA_DRIVER_NAME = "nvidia";      
+    NIXOS_OZONE_HLWM = "1";            
+    MOZ_ENABLE_WAYLAND = "1";          
+    GTK_BACKEND = "wayland";           
+    QT_QPA_PLATFORM = "wayland;xcb";   
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1"; 
+    SDL_VIDEODRIVER = "wayland";       
+    _JAVA_AWT_WM_NONREPARENTING = "1"; 
+  };
+
+  # Core Minimal Desktop Tooling Block
+  environment.systemPackages = with pkgs; [
+    alacritty       
+    xdg-utils       
+    fuzzel          
+  ];
+}
