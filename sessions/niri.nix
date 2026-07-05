ed61dfec2134d15083e3ff565a4d40ca6dba1d4e
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-05 06:11:11 +0000

    Update niri.nix

--- sessions/niri.nix
+++ sessions/niri.nix
@@ -10,15 +10,17 @@
       niri = {
         prettyName = "Niri";
         comment = "Niri scrollable-tiling compositor managed by UWSM";
-        binPath = "${lib.getExe pkgs.niri}";
-        extraArgs = [ "--session" ];
+        binPath = lib.getExe pkgs.niri;
       };
     };
   };
 
-  environment.sessionVariables = lib.mkForce {};
-
-  systemd.user.settings.Manager = {
-    DefaultEnvironment = "\"WLR_NO_HARDWARE_CURSORS=1\" \"_JAVA_AWT_WM_NONREPARENTING=1\" \"SDL_VIDEODRIVER=wayland,x11\" \"QT_QPA_PLATFORM=wayland;xcb\"";
+  # Enterprise Standard: Define variables natively as an attribute set.
+  # UWSM will automatically inject these into the Wayland session and Systemd user instance.
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    SDL_VIDEODRIVER = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
 }
