db3e5e44abffaae0d810a103caace1a57842f3f8
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-07 09:56:15 +0000

    Update desktop-applications.nix

--- packages/desktop-applications.nix
+++ packages/desktop-applications.nix
@@ -1,13 +1,15 @@
-# ~/nix-btw/packages/desktop-applications.nix
 { pkgs, ... }:
 
 {
-  # Desktop Applications (user-space)
-  home.packages = [
-    pkgs.google-chrome  # Web browser (Standard Chromium-based)
-    pkgs.keepassxc      # Password Manager (Offline, encrypted local-only vault)
-    pkgs.min            # Web browser (Minimalist, privacy-focused)
-    pkgs.nautilus       # File Manager (GNOME core utility)
-    pkgs.obsidian       # Markdown Editor (Knowledge base)
-  ];
+  # Wrap this in a config block to tell Nix this is implementation metadata
+  config = {
+    # Desktop Applications (user-space)
+    home.packages = [
+      pkgs.google-chrome  # Web browser (Standard Chromium-based)
+      pkgs.keepassxc      # Password Manager (Offline, encrypted local-only vault)
+      pkgs.min            # Web browser (Minimalist, privacy-focused)
+      pkgs.nautilus       # File Manager (GNOME core utility)
+      pkgs.obsidian       # Markdown Editor (Knowledge base)
+    ];
+  };
 }
